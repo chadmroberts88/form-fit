0c0db22e025105ee1deff2dddcf404daa1ffed13
chadmroberts88 <chadmroberts88@gmail.com>
2022-11-27 11:28:56 -0330

    Added tensorflow and pose detection.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
+import Webcam from 'react-webcam';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
 
 function App() {
+  const webcamRef = useRef<any>(null);
+  const canvasRef = useRef<any>(null);
+
+  const runPosenet = async () => {
+    const model = poseDetection.SupportedModels.MoveNet;
+    const config = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
+    const detector = await poseDetection.createDetector(model, config);
+
+    setInterval(() => {
+      detect(detector);
+    }, 100);
+  };
+
+  const detect = async (detector: poseDetection.PoseDetector) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video: HTMLVideoElement = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      const pose = await detector.estimatePoses(video);
+      console.log(pose[0]);
+    }
+  };
+
+  runPosenet();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+        id="video"
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
     </div>
   );
 }
