100df4c8009e378e43982e8da69b11124f235a79
chadmroberts88 <chadmroberts88@gmail.com>
2022-11-28 09:19:03 -0330

    Added function to draw whole skeleton to canvas.

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,25 @@ import './App.css';
 import Webcam from 'react-webcam';
 import { Keypoint } from '@tensorflow-models/pose-detection';
 import * as poseDetection from '@tensorflow-models/pose-detection';
-import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
 
 function App() {
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const SCORE_THRESHOLD = 0.3;
 
   const runPoseDetection = async () => {
     const model = poseDetection.SupportedModels.MoveNet;
     const config = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
     const detector = await poseDetection.createDetector(model, config);
+    const adjacentPairs = poseDetection.util.getAdjacentPairs(model);
 
     setInterval(() => {
-      detect(detector);
+      detect(detector, adjacentPairs);
     }, 100);
   };
 
-  const detect = async (detector: poseDetection.PoseDetector) => {
+  const detect = async (detector: poseDetection.PoseDetector, adjacentPairs: number[][]) => {
     if (
       typeof webcamRef.current !== undefined &&
       webcamRef.current !== null &&
@@ -34,27 +35,61 @@ function App() {
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      const pose = await detector.estimatePoses(video);
-      drawKeypoints(pose[0]?.keypoints, videoWidth, videoHeight);
+      try {
+        const pose = await detector.estimatePoses(video);
+        drawSkeleton(pose[0]?.keypoints, adjacentPairs, videoWidth, videoHeight);
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
-  const drawKeypoints = (keypoints: Keypoint[], videoWidth: number, videoHeight: number) => {
+  const drawSkeleton = (
+    keypoints: Keypoint[],
+    adjacentPairs: number[][],
+    videoWidth: number,
+    videoHeight: number
+  ) => {
     if (
       typeof canvasRef.current !== undefined &&
       canvasRef.current !== null &&
-      keypoints !== undefined
+      keypoints !== undefined &&
+      adjacentPairs !== undefined
     ) {
       const canvas: HTMLCanvasElement = canvasRef.current;
       const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
       canvas.width = videoWidth;
       canvas.height = videoHeight;
 
+      // draw lines
+      adjacentPairs.forEach((pair: number[]) => {
+        const keypointA: Keypoint = keypoints[pair[0]];
+        const keypointB: Keypoint = keypoints[pair[1]];
+
+        const scoreA = keypointA.score !== null ? keypointA.score : 1;
+        const scoreB = keypointB.score !== null ? keypointB.score : 1;
+
+        if (
+          ctx !== null &&
+          scoreA !== undefined &&
+          scoreB !== undefined &&
+          scoreA >= SCORE_THRESHOLD &&
+          scoreB >= SCORE_THRESHOLD
+        ) {
+          ctx.beginPath();
+          ctx.moveTo(keypointA.x, keypointA.y);
+          ctx.lineTo(keypointB.x, keypointB.y);
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'white';
+          ctx.stroke();
+        }
+      });
+
+      // draw points
       keypoints.forEach((keypoint: Keypoint) => {
         const score = keypoint.score !== null ? keypoint.score : 1;
-        const scoreThreshold = 0.3;
 
-        if (ctx !== null && score !== undefined && score >= scoreThreshold) {
+        if (ctx !== null && score !== undefined && score >= SCORE_THRESHOLD) {
           ctx.beginPath();
           ctx.arc(keypoint.x, keypoint.y, 6, 0, 2 * Math.PI);
           ctx.fillStyle = '#4BB543';
